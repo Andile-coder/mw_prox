f4d890b6d500b58ef59129b5497a18e443688802
Andile-coder <maselaandile@gmail.com>
2022-07-14 20:49:52 +0200

    node server working

---
+++ newSql.sql
@@ -0,0 +1,35 @@
+
+drop owned by andilemasela;
+CREATE TYPE typeInstance AS ENUM ('lxc','qemu');
+
+CREATE TABLE Clusters(
+    id      SERIAL PRIMARY KEY,
+    name    varchar(100) NOT NULL UNIQUE,
+    description varchar(200),
+    baseURL varchar(100)
+);
+CREATE TABLE Nodes(
+    id SERIAL PRIMARY KEY,
+    clusterId varchar REFERENCES Clusters(name),
+    nodeId VARCHAR(20) NOT NULL UNIQUE,
+    status varchar(20)
+);
+
+CREATE TABLE Insantces(
+    id      SERIAL PRIMARY KEY,
+    vmid    VARCHAR(20) NOT NULL,
+    name    VARCHAR(20) NOT NULL,
+    node_id VARCHAR REFERENCES Nodes(nodeId),
+    intanceType typeInstance,
+    cores   INT,
+    memory  INT,
+    status  varchar(20),
+    networks JSON,
+    storage JSON
+);
+
+INSERT INTO Clusters (name,description,baseURL) VALUES('cluster name','cluster description','cluster url');
+INSERT INTO Nodes (clusterId,nodeId,status) VALUES('cluster name','nodeDemoId','status');
+INSERT INTO Insantces (vmid,name,node_id,intanceType,cores,memory,status,networks,storage) VALUES('vmid','instanceName','nodeDemoId','lxc',23,100,'demostatus','[{"size":"gbo"},{"size":"gb"}]','[{"size":"gbo"},{"size":"gb"}]');
+
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,60 @@
 const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+  user: "andilemasela",
+  host: "localhost",
+  database: "mw_prox",
+  password: "Masela@2000",
+  port: 5432,
+});
+
+app.get("/instances", (req, res) => {
+  pool
+    .query("SELECT * FROM Insantces")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+app.get("/nodes", (req, res) => {
+  pool
+    .query("SELECT * FROM Nodes")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+app.post("/cluster", (req, res) => {
+  const name = "nodejsDemo";
+  const description = "description2";
+  const baseURL = "clusterUrl";
+
+  const query =
+    "INSERT INTO Clusters (name,description,baseURL) VALUES($1,$2,$3)";
+  pool
+    .query(query, [name, description, baseURL])
+    .then(() => res.send("cluster added"))
+    .catch((error) => console.log(error));
+});
+
+app.get("/cluster", (req, res) => {
+  const query = "SELECT * FROM Nodes";
+  pool
+    .query(query)
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Listening to PORT 3000");
+});
